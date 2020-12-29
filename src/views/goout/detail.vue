ba798d51ae18223d3d03bf17bce41b2581d14544
dongxiuling <644961699@qq.com>
2020-12-29 18:13:01 +0800

    设置进入离开默认时间

--- src/views/goout/detail.vue
+++ src/views/goout/detail.vue
@@ -9,38 +9,44 @@
         <el-form :rules="rules" ref="form" :model="todaywork" label-width="100px" size="medium">
           <el-form-item label="联系人：">{{ todaywork.contacts }}</el-form-item>
           <el-form-item label="来访事由：">{{ todaywork.title }}</el-form-item>
-          <el-form-item label="开始时间：">{{ todaywork.startTime }}</el-form-item>
-          <el-form-item label="结束时间：">{{ todaywork.endTime }}</el-form-item>
+          <el-form-item label="来访时段">{{ todaywork.startTime }} 至 {{ todaywork.endTime }}</el-form-item>
           <el-form-item label="来访人员：">
-            <div
-              class="out-item"
-              v-for="(obj,index) in todaywork.persons"
-              :key="index"
-            >{{obj.name}}
-            身份证号：{{obj.idCard}}
-            电话：{{obj.tel}}
+            <div class="out-item" v-for="(obj, index) in todaywork.persons" :key="index">
+              姓名：{{
+              obj.name
+              }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;手机号：{{
+              obj.tel
+              }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;身份证号：{{
+              obj.idCard
+              }}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <el-button
+                @click="openTabWin(obj.url, 'view')"
+                v-if="obj.url"
+                icon="el-icon-view"
+                size="small"
+                type="primary"
+              >查看附件</el-button>
             </div>
           </el-form-item>
           <el-form-item label="来访车辆：">
             <span class="out-item" v-for="(obj,index) in todaywork.cars" :key="index">{{obj.carNum}}</span>
           </el-form-item>
           <el-form-item label="备注:">{{ todaywork.remark }}</el-form-item>
-          <el-form-item label="进入时间:" prop="enterTime">
+          <el-form-item label="进入时间:">
             <el-date-picker
-              v-model="todaywork.enterTime"
-              :default-value="new Date()"
+              v-model="enterTime"
               type="datetime"
               placeholder="选择日期时间"
               value-format="yyyy-MM-dd hh:mm:ss"
             ></el-date-picker>
             <el-button type="warning" @click="enter(1)">进入</el-button>
           </el-form-item>
-          <el-form-item label="离开时间:" prop="leaveTime">
+          <el-form-item label="离开时间:">
             <el-date-picker
-              v-model="todaywork.leaveTime"
+              v-model="leaveTime"
               type="datetime"
               placeholder="选择日期时间"
-              :default-value="new Date()"
               value-format="yyyy-MM-dd hh:mm:ss"
             ></el-date-picker>
             <el-button type="success" @click="enter(2)">离开</el-button>
@@ -56,27 +62,15 @@
 </template>
 
 <script>
-import {dateFormat} from '@/utils/format.js'
+import { dateFormat } from "@/utils/format.js";
 import { getDetailById, saveRecord } from "@/api/goout.js";
 export default {
   data() {
     return {
       id: this.$route.query.id,
-      todaywork: {
-        enterTime:'2020-10-20 12:00:01',
-        leaveTime:new Date()
-      },
-      fullscreenLoading: true,
-      // enterTime: "",
-      // leaveTime: "",
-      rules: {
-        enterTime: [
-          { required: true, message: "请输入进入时间", trigger: "blur" }
-        ],
-        leaveTime: [
-          { required: true, message: "请输入离开时间", trigger: "blur" }
-        ]
-      }
+      enterTime: "",
+      leaveTime: "",
+      fullscreenLoading: true
     };
   },
   methods: {
@@ -88,8 +82,7 @@ export default {
       }
     },
     async enter(type) {
-      let time =
-        type == 1 ? this.todaywork.enterTime : this.todaywork.leaveTime;
+      let time = type == 1 ? this.enterTime : this.leaveTime;
       // 调用进入接口
       const res = await saveRecord({
         time,
@@ -98,14 +91,45 @@ export default {
       });
       if (res.code === "200") {
         this.$message({
-          message: "进入记录成功",
+          message: "记录成功",
           type: "success"
         });
       }
+    },
+    async openTabWin(url, type) {
+      // if (type == "view") {
+      //   await readFile({ id: this.file.id })
+      // } else if (type = "download") {
+      //   await downLoadFile({ id: this.file.id })
+      // }
+      window.open(url, "_blank");
+    },
+    async initTime() {
+      //获取当前时间
+      var date = new Date(); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+      var Y = date.getFullYear() + "-";
+      var M =
+        (date.getMonth() + 1 < 10
+          ? "0" + (date.getMonth() + 1)
+          : date.getMonth() + 1) + "-";
+      var D =
+        (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + " ";
+      var h =
+        (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
+      var m =
+        (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
+        ":";
+      var s =
+        date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+      let strDate = Y + M + D + h + m + s;
+      this.enterTime = strDate;
+      this.leaveTime = strDate;
     }
   },
   created() {
     this.getListDetail();
+    this.initTime();
   }
 };
 </script>
@@ -122,6 +146,6 @@ export default {
   margin-top: 20px;
 }
 .out-item {
-  margin-right:  10px;
+  margin-right: 10px;
 }
 </style>
